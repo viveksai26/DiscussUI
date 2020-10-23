9fa8480fab6b8adbae351f46da4db0524cece415
Vivek <you@example.com>
2020-10-23 21:41:56 +0530

    Text

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent implements OnInit {
   constructor(private firebaseService: FirebaseService) {}
   ngOnInit() {}
   login() {
+    console.log('clicked login'); 
     this.firebaseService.login();
   }
 }
